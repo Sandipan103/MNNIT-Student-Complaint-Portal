99baac9a31c02812bf1ba6f1566730c283582eae
Sandipan19000 <sarkarsandipan19000@gmail.com>
2024-02-09 02:21:50 +0530

    frontend connect with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import ComplaintForm from "./pages/ComplaintForm";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div>
-      <h1> Mini Project </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/complaintForm" element={<ComplaintForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
